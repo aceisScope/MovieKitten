d9ed973ac68f769d24bd0eb42003ed5b8a6a5b46
Binghui Liu <bertieliu52@gmail.com>
2017-01-31 21:13:27 +0200

    Initial Commit

---
+++ MovieKitten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieKitten
+//
+//  Created by Binghui Liu on 2017/1/31.
+//  Copyright © 2017年 Binghui Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
